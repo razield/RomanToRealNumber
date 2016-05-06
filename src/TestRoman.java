7b9002c52df094f006562c623502bc477d4a7a22
Juan Felipe <philip217@gmail.com>
2016-05-06 14:09:59 -0500

    falla prueba 2

--- src/TestRoman.java
+++ src/TestRoman.java
@@ -5,11 +5,14 @@ public class TestRoman {
 	
 	
 	@org.junit.Test
-	public void isSecretNumber() {
-		
+	public void theNumberIsI() {
 		RomanNumber romanNumber = new RomanNumber("I");
-		
 		assertEquals(romanNumber.transform(), 1 );
+	}
+	@org.junit.Test
+	public void theNumberIsII(){
+		RomanNumber romanNumber = new RomanNumber("II");
+		assertEquals(romanNumber.transform(), 2);
 	}
 
 }
