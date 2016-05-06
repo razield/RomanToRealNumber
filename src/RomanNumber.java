a2902ff13489d0b4a915c4455095dbe3a73ec7d4
Juan Felipe <philip217@gmail.com>
2016-05-06 14:29:31 -0500

    se crea funcion whatIsTheNumbe para saber los numeros principales por cada letra

--- src/RomanNumber.java
+++ src/RomanNumber.java
@@ -13,4 +13,25 @@ public class RomanNumber {
 		}
 		return number;
 	}
+	
+	public int whatIsTheNumber(char number){
+		
+		switch(number){
+			case 'I':
+				return 1;
+			case 'V':
+				return 5;
+			case 'X':
+				return 10;
+			case 'L':
+				return 50;
+			case 'C':
+				return 100;
+			case 'D':
+				return 500;
+			case 'M':
+				return 1000;
+		}
+		return 0;
+	}
 }
